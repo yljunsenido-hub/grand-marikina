fc103a5ee1964b8a85c5994303f805fd8e6b2f2c
yl <plukyljun@gmail.com>
2026-05-14 15:50:06 +0800

    Re-add prisma config for Vercel build

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  datasource: {
-    // By using process.env directly, it won't crash if Vercel hasn't loaded it yet during the install phase!
-    url: process.env.POSTGRES_URL_NON_POOLING || "",
-  }
-});
