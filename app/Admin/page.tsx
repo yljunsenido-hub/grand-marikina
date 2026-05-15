b680ed582e445eeb5ff03216a5899125fa3590b9
yl <plukyljun@gmail.com>
2026-05-15 12:43:45 +0800

    Resize and Reposition User Button

--- app/Admin/page.tsx
+++ app/Admin/page.tsx
@@ -17,7 +17,8 @@ export default async function AdminDashboard() {
         <UserButton 
           appearance={{
             elements: {
-              userButtonAvatarBox: "w-28 h-28" // Increases size to 56px (default is much smaller!)
+              userButtonAvatarBox: "!w-20 !h-20", 
+              userButtonTrigger: "!w-20 !h-20" // Increases size to 56px (default is much smaller!)
             }
           }}
         /> 
